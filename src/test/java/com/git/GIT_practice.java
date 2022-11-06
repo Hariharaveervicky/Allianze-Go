2e93c1d7a12b87ec381f6afad4cefab6d082d5da
Hariharaveervicky <hari.info2vicky.tecch@gmail.com>
2022-11-06 19:04:34 +0530

    fist push

---
+++ src/test/java/com/git/GIT_practice.java
@@ -0,0 +1,16 @@
+package com.git;
+
+public class GIT_practice {
+	public void Tool1() {
+		System.out.println("tool1");
+}
+	public void Tool2() {
+		System.out.println("tool2");
+}
+	public void Tool3() {
+System.out.println("tool3");
+}
+	public void Tool4() {
+System.out.println("tool4");
+}
+}
